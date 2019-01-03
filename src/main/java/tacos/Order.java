ca395b02107be944ce352cd9c461b91cfdf127e6
michaelschumacher777 <michael.j.schumacher777@gmail.com>
2019-01-03 13:15:57 -0600

    Store latest changes - page 45 in book

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,15 +1,44 @@
-package tacos.domain;
+package tacos;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.*;
 
 public class Order {
 
+    @NotNull(message = "Name is required")
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
+    @NotBlank(message = "Street is required")
     private String street;
+    @NotBlank(message = "Cit is required")
     private String city;
+    @NotBlank(message = "State is required")
     private String state;
+    @NotBlank(message = "Zip is required")
     private String zip;
+    @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+        message = "Must be formatted MM/YY")
     private String ccExpiration;
+    @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "name='" + name + '\'' +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                ", ccNumber='" + ccNumber + '\'' +
+                ", ccExpiration='" + ccExpiration + '\'' +
+                ", ccCVV='" + ccCVV + '\'' +
+                '}';
+    }
 
     public String getName() {
         return name;
